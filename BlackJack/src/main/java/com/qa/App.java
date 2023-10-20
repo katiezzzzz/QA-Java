35a7a9feb78ef2b191f713d305fd3afb655e3c66
Charles Watson <charlie.g.watson@gmail.com>
2023-10-20 15:29:13 +0100

    added blackjack skeleton

---
+++ BlackJack/src/main/java/com/qa/App.java
@@ -0,0 +1,19 @@
+package com.qa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to proper Black Jack!!! :)" );
+//        ask user for number of players
+
+    }
+
+    private static void initialiseGame() {
+
+    }
+}
